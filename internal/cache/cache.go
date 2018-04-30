c9482111fb054b38eea369f9566abbf505b0d6ba
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2018-04-30 15:49:34 +0200

    Add mutex sync to cache

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -4,24 +4,31 @@ import (
 	"errors"
 	"os"
 	"path/filepath"
+	"sync"
 	"time"
 
 	"github.com/coreos/bbolt"
+)
+
+const (
+	filePerm os.FileMode = 0644
 )
 
 // Cache represents the cache.
 type Cache struct {
 	bucket []byte
 	db     *bolt.DB
+
+	sync.Mutex
 }
 
 // New creates a new cache.
 func New(dir string) (*Cache, error) {
-	if err := os.MkdirAll(dir, os.ModePerm); err != nil {
+	if err := os.MkdirAll(dir, 0750); err != nil {
 		return nil, err
 	}
 
-	db, err := bolt.Open(filepath.Join(dir, "cache.db"), 0600, &bolt.Options{Timeout: 1 * time.Second})
+	db, err := bolt.Open(filepath.Join(dir, "cache.db"), filePerm, &bolt.Options{Timeout: 1 * time.Second})
 
 	if err != nil {
 		return nil, err
@@ -40,6 +47,9 @@ func (c *Cache) Close() error {
 
 // Delete delets a cache value.
 func (c *Cache) Delete(key string) error {
+	c.Lock()
+	defer c.Unlock()
+
 	return c.db.Update(func(tx *bolt.Tx) error {
 		b := tx.Bucket(c.bucket)
 
@@ -53,6 +63,9 @@ func (c *Cache) Delete(key string) error {
 
 // Flush flushes the cache.
 func (c *Cache) Flush() error {
+	c.Lock()
+	defer c.Unlock()
+
 	return c.db.Update(func(tx *bolt.Tx) error {
 		return tx.DeleteBucket([]byte(c.bucket))
 	})
@@ -60,7 +73,10 @@ func (c *Cache) Flush() error {
 
 // Get gets a value from the cache and returns it or a error.
 func (c *Cache) Get(key string) ([]byte, error) {
-	var v []byte
+	var val []byte
+
+	c.Lock()
+	defer c.Unlock()
 
 	err := c.db.View(func(tx *bolt.Tx) error {
 		b := tx.Bucket(c.bucket)
@@ -69,10 +85,13 @@ func (c *Cache) Get(key string) ([]byte, error) {
 			return bolt.ErrBucketNotFound
 		}
 
-		v = b.Get([]byte(key))
+		v := b.Get([]byte(key))
 		if len(v) == 0 {
 			return errors.New("empty cache value")
 		}
+
+		val = make([]byte, len(v))
+		copy(val, v)
 
 		return nil
 	})
@@ -81,11 +100,14 @@ func (c *Cache) Get(key string) ([]byte, error) {
 		return nil, err
 	}
 
-	return v, nil
+	return val, nil
 }
 
 // Set sets a value and return a error if any.
 func (c *Cache) Set(key string, value []byte) error {
+	c.Lock()
+	defer c.Unlock()
+
 	return c.db.Update(func(tx *bolt.Tx) error {
 		b, err := tx.CreateBucketIfNotExists(c.bucket)
 		if err != nil {
